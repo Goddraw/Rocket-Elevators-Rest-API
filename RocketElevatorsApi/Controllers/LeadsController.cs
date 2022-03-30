388398e25753cb8912521c14a1b4dab2348be9e9
goddraw <coolo_ricky@hotmail.com>
2022-03-30 11:20:38 -0400

    almost done with leads controller to find leads in last 30 days, need to test code to make method work

--- RocketElevatorsApi/Controllers/LeadsController.cs
+++ RocketElevatorsApi/Controllers/LeadsController.cs
@@ -47,54 +47,83 @@ namespace RocketElevatorsApi.Controllers
         public async Task<ActionResult<IEnumerable<Lead>>> GetNewLeads()
         {
             List<Lead> allLeads= await _context.leads.ToListAsync();
-            List<User.email> allUsersEmails= await _context.users.email.ToListAsync();
-            List<Customer> allCustomers= await _context.users.ToListAsync();
+            List<User> allUsers = await _context.users.ToListAsync();
+            List<Customer> allCustomers= await _context.customers.ToListAsync();
             List<Lead> potentialCustomers = new List<Lead>();
-            DateTime today = DateTime.Now;
+            List<Lead> recentLeads = new List<Lead>();
+
+// add a column in a table to match 2 tables. Foreign key between Leads and Customers
+            List<long?> allCustomersUserId = new List<long?>();
+            foreach (Customer customer in allCustomers){
+                allCustomersUserId.Add(customer.user_id);
+            }
 
             foreach (Lead lead in allLeads)
             {
-                if (today - lead.created_at <= 30)
+                DateTime today = DateTime.Now;
+                DateTime leadDate = lead.created_at;    
+                if ((today - leadDate).TotalDays < 50)
                 {
-                   if (!allUsersEmails.Contains(allLeads.email))
-                   {
-                       potentialCustomers.Add();
-                   }
-                    foreach (User user in allUsers)
-                    {
-                      if (user.email =! lead.email)
-                      {
-                          potentialCustomers.Add();
-                      }
-                      {
-                        foreach (Customer customer in allCustomers)
-                        {
-                            if (customer.Email_of_the_company_contact == lead.email)
-                            {
-                                return;
-                            }
-                        }
-                     }
+                    recentLeads.Add(lead);
+                    
+                }
+            }
+
+            Console.WriteLine(recentLeads);
+
+            List<String> recentLeadEmails = new List<String>();
+            foreach(Lead lead in recentLeads){
+                recentLeadEmails.Add(lead.email);
+            }
+
+            Console.WriteLine(recentLeadEmails);
+
+            foreach (User user in allUsers){
+                if (!recentLeadEmails.Contains(user.email)){
+                    var lead = await _context.leads.Collection(x => x.email == user.email).LoadAsync();
+                    potentialCustomers.Add(lead);
+                    Console.WriteLine(lead);
+                } else {
+                    if (!allCustomersUserId.Contains(user.Id)){
+                        
+                        var lead = await _context.leads.Where(x => x.email == user.email).LoadAsync();
+                        potentialCustomers.Add(lead);
                     }
                 }
             }
 
-
-            return await _context.leads.ToListAsync();
+            return potentialCustomers;
         }
 
-        // GET: api/Leads/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Lead>> GetLead(long id)
+         // GET: api/Leads. GET 30 last days non-customers lead
+        [HttpGet("recentleads")]
+        public async Task<ActionResult<IEnumerable<Lead>>> GetRecentLeads()
         {
-            var lead = await _context.leads.FindAsync(id);
+            List<Lead> allLeads= await _context.leads.ToListAsync();
+            List<User> allUsers = await _context.users.ToListAsync();
+            List<Customer> allCustomers= await _context.customers.ToListAsync();
+            List<Lead> potentialCustomers = new List<Lead>();
+            List<Lead> recentLeads = new List<Lead>();
 
-            if (lead == null)
-            {
-                return NotFound();
+// add a column in a table to match 2 tables. Foreign key between Leads and Customers
+            List<long?> allCustomersUserId = new List<long?>();
+            foreach (Customer customer in allCustomers){
+                allCustomersUserId.Add(customer.user_id);
             }
 
-            return lead;
+            foreach (Lead lead in allLeads)
+            {
+                DateTime today = DateTime.Now;
+                DateTime leadDate = lead.created_at;    
+                if ((today - leadDate).TotalDays < 50)
+                {
+                    recentLeads.Add(lead);
+                    
+                }
+            }
+
+            Console.WriteLine(recentLeads);
+            return recentLeads;
         }
 
 
